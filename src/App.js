f8a987839051e937ea2985ea445c1fd9f9208b92
hanga <hangajean39@gmail.com>
2022-05-20 14:26:57 -0400

    ajouter de deux routes dans le project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screen/HomeScreem';
+import ProductScreen from './screen/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/"> Shopping Camerounais</a>
-      </header>
-      <main>
-        <h1>Produits disponible</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>Fcfa {product.price}</strong>
-                </p>
-                <button>Ajouter Au Panier</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/"> Shopping Camerounais</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
