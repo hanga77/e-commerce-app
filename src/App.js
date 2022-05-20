6bb629b7c1f8e01d3b8140e9f328a30849478d6d
hanga <hangajean39@gmail.com>
2022-05-20 13:40:42 -0400

    affichage des premiers produits dans mon catalogue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/"> Shopping Camerounais</a>
       </header>
-      <main> list de produits</main>
+      <main>
+        <h1>Produits disponible</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>Fcfa {product.price}</strong>
+                </p>
+                <button>Ajouter Au Panier</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
